961ace82e4636fbbef6173375de68cea466efb9e
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-06-01 23:29:19 -0400

    last commit

--- JavaScriptAxiosLab.js
+++ JavaScriptAxiosLab.js
@@ -17,7 +17,6 @@ const API_KEY = "live_bQoDd1X9QVK1t1ddpGfcOSPbssO6WXEuBiggR5bITIT0chDUMb71X25p2t
 /**
  * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
 */
-
 /**
  * 4. Change all of your fetch() functions to axios!
  * - axios has already been imported for you within index.js.
@@ -27,18 +26,17 @@ const API_KEY = "live_bQoDd1X9QVK1t1ddpGfcOSPbssO6WXEuBiggR5bITIT0chDUMb71X25p2t
  *   by setting a default header with your API key so that you do not have to
  *   send it manually with all of your requests! You can also set a default base URL!
 */
-
-// Part 1 Using Axios instead of fetch
+// Part 4: Using Axios instead of fetch for Part 1 code
 async function initialLoad() {
     try {
         let response = await axios.get("https://api.thecatapi.com/v1/breeds");
         let breeds = response.data;
-        
+
         for (let breed of breeds) {
             let option = document.createElement("option");
             option.setAttribute("value", breed.id)
             option.innerText = breed.name;
-            
+
             breedSelect.appendChild(option);
             console.log(breed);
         }
@@ -48,42 +46,44 @@ async function initialLoad() {
 }
 initialLoad();
 
-// Part 2 Using Axios instead of fetch
+// Part 4: Using Axios instead of fetch for Part 2 code
 breedSelect.addEventListener("change", async (e) => {
     Carousel.clear()
     console.log("Now viewing breed:", e.target.value);
     let url = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${e.target.value}&api_key=${API_KEY}`
-    
+
     try {
-        
-        let response = await axios.get(url)
+
+        let response = await axios.get(url, {
+            onDownloadProgress: updateProgress
+        })
         let data = response.data
-        
+
         for (let item of data) {
-            
+
             let src = item.url
             let alt = item.breeds[0].name
             let id = item.id
-            
-            
+
+
             let carouselItem = Carousel.createCarouselItem(src, alt, id)
             Carousel.appendCarousel(carouselItem)
-            
+
         }
-        
+
         let info = data[0].breeds[0].description
         let description = document.createElement("info");
         description.setAttribute("innerText", info)
         description.innerText = info
-        
+
         infoDump.appendChild(description)
-        
+
         console.log(data)
-        
+
     } catch (error) {
         console.log(error)
     }
-    
+
 })
 /**
  * 5. Add axios interceptors to log the time between request and response to the console.
@@ -91,6 +91,34 @@ breedSelect.addEventListener("change", async (e) => {
  * - Add a console.log statement to indicate when requests begin.
  * - As an added challenge, try to do this on your own without referencing the lesson material.
 */
+axios.interceptors.request.use(function (config) {
+    document.body.style.cursor = 'progress';
+
+    progressBar.style.width = '0%'
+
+    config.metadata = config.metadata || {};
+    config.metadata.startTime = new Date().getTime();
+    console.log('Request has begun')
+    return config;
+});
+
+axios.interceptors.response.use(function (config) {
+
+    document.body.style.cursor = 'default';
+
+    config.config.metadata.endTime = new Date().getTime();
+    config.config.metadata.durationInMS = config.config.metadata.endTime - config.config.metadata.startTime;
+
+    console.log(`Request completed in: ${config.config.metadata.durationInMS} milliseconds.`)
+    return config;
+
+}, function (error) {
+    error.config.metadata.endTime = new Date().getTime();
+    error.config.metadata.durationInMS = error.config.metadata.endTime - error.config.metadata.startTime;
+
+    console.log(`Request completed in: ${error.config.metadata.durationInMS} milliseconds.`)
+    throw error;
+});
 /**
  * 6. Next, we'll create a progress bar to indicate the request is in progress.
  * - The progressBar element has already been created for you.
@@ -106,32 +134,21 @@ breedSelect.addEventListener("change", async (e) => {
  *   once or twice per request to this API. This is still a concept worth familiarizing yourself
  *   with for future projects.
 */
-axios.interceptors.request.use(function (config) {
-    config.metadata = config.metadata || {};
-    config.metadata.startTime = new Date().getTime();
-    console.log('Request has begun')
-    return config;
-});
+function updateProgress(progressEvent) {
+    console.log(progressEvent);
+    
+    const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
 
-axios.interceptors.response.use(function (config) {
-        config.config.metadata.endTime = new Date().getTime();
-        config.config.metadata.durationInMS = config.config.metadata.endTime - config.config.metadata.startTime;
+    progressBar.style.width = `${percentCompleted}%`;
 
-        console.log(`Request took ${config.config.metadata.durationInMS} milliseconds.`)
-        return config;
-    }, function (error){
-        error.config.metadata.endTime = new Date().getTime();
-        error.config.metadata.durationInMS = error.config.metadata.endTime - error.config.metadata.startTime;
-
-        console.log(`Request took ${error.config.metadata.durationInMS} milliseconds.`)
-        throw error;
-});
+}
 
 /**
  * 7. As a final element of progress indication, add the following to your axios interceptors:
  * - In your request interceptor, set the body element's cursor style to "progress."
  * - In your response interceptor, remove the progress cursor style from the body element.
  */
+
 /**
  * 8. To practice posting data, we'll create a system to "favourite" certain images.
  * - The skeleton of this function has already been created for you.
@@ -144,7 +161,17 @@ axios.interceptors.response.use(function (config) {
  * - You can call this function by clicking on the heart at the top right of any image.
  */
 export async function favourite(imgId) {
-  // your code here
+    // your code here
+    try {
+        await axios.post("https://api.thecatapi.com/v1/favourites", { image_id: imgId }, {headers: {
+            'x-api-key': API_KEY} 
+        })
+
+        console.log(`Favorited img: ${imgId}`)
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 /**
